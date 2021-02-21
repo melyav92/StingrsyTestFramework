3f3d41a31ce563ddb04d3fa8bcc94b8c17560ea4
vmelnyk <melyav92@gmail.com>
2021-02-21 22:19:25 +0200

    1. Login to SCP 2. Open 'Camera' menu item on header 3. Open 'Register' menu item on header 4. Navigating to the Manual count register page

--- test/specs/manual-lice-count-report-register.js
+++ test/specs/manual-lice-count-report-register.js
@@ -1,22 +1,31 @@
 const {assert} = require('chai');
 
 describe('Manual lice count register ', () => {
-    it('Manual lice count report is successfully saved', () => {
+    it('Open Manual lice count report is successfully saved', () => {
         browser.url ('/');
         $('#Username').addValue('bolacslu');
         $('#Password').addValue(123456);
         $('#login-button').click();
-        let register = $('.dropdown-toggle, .arrow').getText()
-        console.log(register)
-        $('.dropdown-toggle, .arrow').click();
-        browser.pause(2000);
-            // register.click()
+        const cameramenuitem = $('.dropdown-toggle, .arrow').getText()
+        console.log(cameramenuitem)
 
-         //let registermanualcount = $('=RegisterManual').getAttribute('href')
-        //console.log(registermanualcount)
-        //register.click()
+        $('.dropdown-toggle, .arrow').click(); //click on the 'Camera' menu item
+        browser.pause(1000);
 
-        assert.strictEqual(register, 'Register')
+        const registermenuitemname = $('//*[@id="main-nav-menu-collapse"]/ul/li[3]/a').getText()
+        console.log(registermenuitemname)
+
+
+        const registerlink = $('//*[@id="main-nav-menu-collapse"]/ul/li[3]')
+        registerlink.click(); //click on the 'Register' menu item
+        browser.pause(1000);
+
+        const registermanualcountlink = $('//*[@id="main-nav-menu-collapse"]/ul/li[3]/ul/li[4]');
+        registermanualcountlink.click(); //click on the 'Register manual count' menu item
+        browser.pause(4000);
+
+
+        assert.strictEqual(registermenuitemname, 'REGISTER')
     })
 })
 
