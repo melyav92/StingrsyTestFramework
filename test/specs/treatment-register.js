da2cebce346986d4d3ee6fb64218c13c8a1eb57f
vmelnyk <melyav92@gmail.com>
2021-02-24 21:01:06 +0200

    added treatment report registering

--- test/specs/treatment-register.js
+++ test/specs/treatment-register.js
@@ -1,30 +1,70 @@
 const {assert} = require('chai');
 
-describe('Treatment count register ', () => {
-    it('Register treatment count for the location', () => {
+describe('Treatment register page  ', () => {
+    it('Treatment report is saved', () => {
         browser.url ('/');
         $('#Username').addValue('bolacslu');
         $('#Password').addValue(123456);
         $('#login-button').click();
+        browser.pause(1000);
 
         const registerDropdown  = $('a*=Register') // find 'a' element by text tretment
         console.log(registerDropdown.getText()) // outputs: "Register" - item name
         registerDropdown.click()
-        browser.pause(1500);
+        browser.pause(1000);
+
 
         const treatmentMenuItem = $('a*=Treatment') // find 'a' element by text treatment
         console.log(treatmentMenuItem.getText()) // outputs: "Treatment" - item name
         console.log(treatmentMenuItem.getAttribute('href')) // outputs: "https://192.168.10.49:8100/en/Treatment/Register"
         treatmentMenuItem.click()
+        browser.pause(1000);
+
+        const addNewTreatmentButton = $('#add-new-treatment-btn') // find 'addNewTreatmentButton
+        addNewTreatmentButton.click()
+        browser.pause(1000);
+
+        const openDatePicker = $('#counted-date-date-picker') // find 'addNewTreatmentButton
+        openDatePicker.click()
+        //browser.pause(1000);
+
+        openDatePicker.addValue(1)
+        console.log(openDatePicker)
+
+        const pensDropdown = $('#pens_selector_chosen')
+        pensDropdown.click()
+
+        const penM1 = $('li*=M1')
+        console.log(penM1.getText())
+        penM1.click()
+
+        const openTreatmentDropdown = $('#treatment-selector-for-all-pens')
+        openTreatmentDropdown.click()
+
+        const selectTreatmentType = $('option*=Slice')
+        console.log(selectTreatmentType.getText())
+        selectTreatmentType.click()
+
+        const addPens = $('#add-new-treatment-for-pens-btn')
+        addPens.click()
+
+        const enterFishpenValue = $('.scp-fish-per-pen-input')
+        enterFishpenValue.addValue(1500)
+        console.log(enterFishpenValue.getText())
+
+        const save = $('#save-btn')
+        save.click()
+
+        //const toaster = $('#toast-container')
+        //console.log(toaster.getText())
+
+
         browser.pause(4000);
 
-
-
-        //browser.executeScript("document.getElementById('datepicker').value='02/11/2019'"); //find date
-        //browser.executeScript()
-
-        assert.strictEqual(registerMenuItemName, 'REGISTER')
+        assert.strictEqual(browser.getTitle(), 'Treatment register')
     })
+
+
 })
 
 
